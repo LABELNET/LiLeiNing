7aa22dba09f617cbcf7ecc05bb32144e87254144
LABELNET <1406046087@qq.com>
2016-05-21 14:04:55 +0800

    添加服装测试类

---
+++ ppfz-dao/src/test/java/colthestest/ClothesTest.java
@@ -0,0 +1,32 @@
+package colthestest;
+
+
+import hpu.edu.lileining.dao.clothes.ClothesMapper;
+import hpu.edu.lileining.dao.model.ClothesVo;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+public class ClothesTest {
+
+    private final String APPLIACTION_CONTEXT_LOCATION="classpath:spring/applicationContext-dao.xml";
+
+    private ApplicationContext applicationContext;
+    private ClothesMapper clothesMapper;
+
+    @Before
+    public void setUp(){
+        applicationContext= new ClassPathXmlApplicationContext(APPLIACTION_CONTEXT_LOCATION);
+        clothesMapper= (ClothesMapper) applicationContext.getBean("clothesMapper");
+    }
+
+    @Test
+    public void getClothesListTest(){
+        List<ClothesVo> clothesList = clothesMapper.getClothesList(0, 10);
+        System.out.println(clothesList.toString());
+    }
+
+}
